06370cacf826d6ff71e8658e7448742f41aa8013
Joel Niedfeldt <joel@niedfeldt.com>
2017-04-13 10:54:34 -0400

    ios target

---
+++ Framework/EAIntroView/EAIntroView-iOS/EAIntroView-iOS.h
@@ -0,0 +1,19 @@
+//
+//  EAIntroView-iOS.h
+//  EAIntroView-iOS
+//
+//  Created by Joel Niedfeldt on 4/13/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EAIntroView-iOS.
+FOUNDATION_EXPORT double EAIntroView_iOSVersionNumber;
+
+//! Project version string for EAIntroView-iOS.
+FOUNDATION_EXPORT const unsigned char EAIntroView_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAIntroView_iOS/PublicHeader.h>
+
+
---
+++ Framework/EAIntroView/EAIntroView/EAIntroView.h
@@ -0,0 +1,19 @@
+//
+//  EAIntroView.h
+//  EAIntroView
+//
+//  Created by Joel Niedfeldt on 4/13/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EAIntroView.
+FOUNDATION_EXPORT double EAIntroViewVersionNumber;
+
+//! Project version string for EAIntroView.
+FOUNDATION_EXPORT const unsigned char EAIntroViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAIntroView/PublicHeader.h>
+
+
