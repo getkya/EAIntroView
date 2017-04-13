b0476508a755c470130076142f9c2776bb528968
Joel Niedfeldt <joel@niedfeldt.com>
2017-04-13 12:08:39 -0400

    attempting to create EAIntroView framework

---
+++ Framework/EARestrictedScrollView/EARestrictedScrollView/EARestrictedScrollView.h
@@ -0,0 +1,19 @@
+//
+//  EARestrictedScrollView.h
+//  EARestrictedScrollView
+//
+//  Created by Joel Niedfeldt on 4/13/17.
+//  Copyright © 2017 ealeksandrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EARestrictedScrollView.
+FOUNDATION_EXPORT double EARestrictedScrollViewVersionNumber;
+
+//! Project version string for EARestrictedScrollView.
+FOUNDATION_EXPORT const unsigned char EARestrictedScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EARestrictedScrollView/PublicHeader.h>
+
+
